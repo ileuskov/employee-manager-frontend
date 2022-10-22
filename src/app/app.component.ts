6caa9e4cdde89eb662f934bb3980fd1b22c1d47f
Ivan Leuskov <ileuskov@gmail.com>
2022-10-22 18:59:17 +0200

    feat: create functions that use service to make http calsl to the backend to manage employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,116 @@
-import { Component } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'employee-manager-frontend';
+export class AppComponent implements OnInit {
+  title: string = 'employee-manager-frontend';
+  employees: Employee[] = [];
+  editEmployee!: Employee | undefined;
+  deleteEmployee!: Employee | undefined;
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    //call getEmployees() that
+    // =>  will call the service that
+    // =>  calls our backend after the app component is initialized
+    this.getEmployees();
+  }
+
+  getEmployees(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (response: Employee[]) => {
+        this.employees = response;
+        console.log(response);
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      },
+    });
+  }
+  onAddEmployee(addForm: NgForm): void {
+    document.getElementById('add-employee-form')!.click();
+    this.employeeService.addEmployee(addForm.value).subscribe({
+      next: (response: Employee) => {
+        console.log(response);
+        // call getEmployees to show updated version of the backend
+        this.getEmployees();
+        addForm.reset();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      },
+    });
+  }
+
+  onUpdateEmployee(employeeToUpdate: Employee): void {
+    this.employeeService.updateEmployee(employeeToUpdate).subscribe({
+      next: (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      },
+    });
+  }
+
+  onDeleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe({
+      next: (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      },
+    });
+  }
+
+  searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (
+        employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1 ||
+        employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      ) {
+        results.push(employee);
+      }
+      this.employees = results;
+    }
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
+  onOpenModal(mode: string, employee?: Employee): void {
+    const container = document.getElementById('main-container')!;
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      this.editEmployee = employee;
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      this.deleteEmployee = employee;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container.appendChild(button);
+    button.click();
+  }
 }
